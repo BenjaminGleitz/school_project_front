57e8cedff7cf4caad9156b1d44c027f88e595182
benjamingleitz <gleitz.benjamin@gmail.com>
2024-04-28 17:35:44 +0200

    authentication + loginForm + token

--- school_project_front/src/App.tsx
+++ school_project_front/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import Navbar from "./components/navbar/Navbar.tsx";
 import Home from "./pages/Home.tsx";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Login from "./pages/Login.tsx";
 
 const App: React.FC = () => {
     return (
@@ -13,6 +14,7 @@ const App: React.FC = () => {
                 <section className='content' id='content'>
                     <Routes>
                         <Route path="/" element={<Home/>}/>
+                        <Route path="/login" element={<Login/>}/>
                     </Routes>
                 </section>
             </BrowserRouter>
--- school_project_front/src/services/getEvent/UseGetAllEvents.tsx
+++ school_project_front/src/services/getEvent/UseGetAllEvents.tsx
@@ -1,13 +1,20 @@
 import {useEffect, useState} from "react";
 import axios from 'axios';
 import Event from "../../types/Event.tsx";
+import {getToken} from "../authentication/TokenLocalStorageService.tsx";
 
 const useGetAllEvents = () => {
     const [events, setEvents] = useState<Event[]>([]);
+    const token = getToken();
 
     const getEvents = async () => {
         try {
-            const response = await axios.get('http://127.0.0.1:8000/api/event/');
+            console.log(token)
+            const response = await axios.get('http://127.0.0.1:8000/api/event/', {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
             setEvents(response.data);
         } catch (error) {
             console.error(error);
