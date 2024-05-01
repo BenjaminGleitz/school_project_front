c8ebce93468d5c8e9331ecb0c27f2b606c0c2b86
benjamingleitz <gleitz.benjamin@gmail.com>
2024-05-01 17:06:06 +0200

    validation html

--- school_project_front/src/components/forms/register/RegisterForm.tsx
+++ school_project_front/src/components/forms/register/RegisterForm.tsx
@@ -3,6 +3,15 @@ import AuthService from "../../../services/authentication/AuthService.tsx";
 import useGetAllCountries from "../../../services/getCountry/UseGetAllCountries.tsx";
 import City from "../../../types/City.tsx";
 
+interface FormErrors {
+    username: string;
+    password: string;
+    firstname: string;
+    lastname: string;
+    selectedCountry: string;
+    general?: string; // Ajout de la propriété général
+}
+
 const RegisterForm: React.FC = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
@@ -11,7 +20,13 @@ const RegisterForm: React.FC = () => {
     const { countries } = useGetAllCountries();
     const [selectedCountry, setSelectedCountry] = useState<number | "">("");
     const [selectedCity, setSelectedCity] = useState<City | null>(null);
-    const [error, setError] = useState("");
+    const [errors, setErrors] = useState<FormErrors>({
+        username: "",
+        password: "",
+        firstname: "",
+        lastname: "",
+        selectedCountry: ""
+    });
 
     const authService = AuthService.getInstance();
 
@@ -20,7 +35,7 @@ const RegisterForm: React.FC = () => {
             await authService.register(username, password, firstname, lastname, selectedCity?.id || null);
             window.location.href = "/login";
         } catch (error) {
-            setError("Erreur lors de l'inscription.");
+            setErrors((prevErrors) => ({ ...prevErrors, general: "Erreur lors de l'inscription." }));
         }
     };
 
@@ -36,57 +51,124 @@ const RegisterForm: React.FC = () => {
         setSelectedCity(city);
     };
 
+    const validateForm = () => {
+        const newErrors = { ...errors };
+        if (!username.trim()) {
+            newErrors.username = "Veuillez saisir votre email.";
+        } else {
+            newErrors.username = "";
+        }
+
+        if (!password.trim()) {
+            newErrors.password = "Veuillez saisir votre mot de passe.";
+        } else if (password.length < 8) {
+            newErrors.password = "Le mot de passe doit contenir au moins 8 caractères.";
+        } else {
+            newErrors.password = "";
+        }
+
+        if (!firstname.trim()) {
+            newErrors.firstname = "Veuillez saisir votre prénom.";
+        } else {
+            newErrors.firstname = "";
+        }
+
+        if (!lastname.trim()) {
+            newErrors.lastname = "Veuillez saisir votre nom.";
+        } else {
+            newErrors.lastname = "";
+        }
+
+        if (!selectedCountry) {
+            newErrors.selectedCountry = "Veuillez sélectionner un pays.";
+        } else {
+            newErrors.selectedCountry = "";
+        }
+
+        setErrors(newErrors);
+
+        return Object.values(newErrors).every((error) => error === "");
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const isValid = validateForm();
+        if (isValid) {
+            handleRegister();
+        }
+    };
+
     return (
         <div>
             <h2>Inscription</h2>
-            <div>
-                <label htmlFor="username">Email :</label>
-                <input
-                    type="text"
-                    id="username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-            </div>
-            <div>
-                <label htmlFor="password">Password :</label>
-                <input
-                    type="password"
-                    id="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-            </div>
-            <div>
-                <label htmlFor="firstname">Firstname :</label>
-                <input
-                    type="text"
-                    id="firstname"
-                    value={firstname}
-                    onChange={(e) => setFirstname(e.target.value)}
-                />
-            </div>
-            <div>
-                <label htmlFor="lastname">Lastname :</label>
-                <input
-                    type="text"
-                    id="lastname"
-                    value={lastname}
-                    onChange={(e) => setLastname(e.target.value)}
-                />
-            </div>
-            <div>
-                <label htmlFor="country">Choisissez votre ville favorite :</label>
-                <select id="country" onChange={handleCountryChange} value={selectedCountry}>
-                    <option value="">Select a country</option>
-                    {countries.map((country) => (
-                        <option key={country.id} value={country.id}>{country.name}</option>
-                    ))}
-                </select>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="username">Email :</label>
+                    <input
+                        type="email"
+                        id="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        required
+                    />
+                    {errors.username && <div>{errors.username}</div>}
+                </div>
+                <div>
+                    <label htmlFor="password">Password :</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                    {errors.password && <div>{errors.password}</div>}
+                </div>
+                <div>
+                    <label htmlFor="firstname">Firstname :</label>
+                    <input
+                        type="text"
+                        id="firstname"
+                        value={firstname}
+                        onChange={(e) => setFirstname(e.target.value)}
+                        required
+                    />
+                    {errors.firstname && <div>{errors.firstname}</div>}
+                </div>
+                <div>
+                    <label htmlFor="lastname">Lastname :</label>
+                    <input
+                        type="text"
+                        id="lastname"
+                        value={lastname}
+                        onChange={(e) => setLastname(e.target.value)}
+                        required
+                    />
+                    {errors.lastname && <div>{errors.lastname}</div>}
+                </div>
+                <div>
+                    <label htmlFor="country">Choisissez votre ville favorite :</label>
+                    <select
+                        id="country"
+                        onChange={handleCountryChange}
+                        value={selectedCountry}
+                        required
+                    >
+                        <option value="">Select a country</option>
+                        {countries.map((country) => (
+                            <option key={country.id} value={country.id}>{country.name}</option>
+                        ))}
+                    </select>
+                    {errors.selectedCountry && <div>{errors.selectedCountry}</div>}
+                </div>
                 {selectedCountry && (
                     <div>
                         <label htmlFor="city">City :</label>
-                        <select id="city" onChange={handleCityChange} value={selectedCity?.id || ""}>
+                        <select
+                            id="city"
+                            onChange={handleCityChange}
+                            value={selectedCity?.id || ""}
+                        >
                             <option value="">Select a city</option>
                             {countries
                                 .find((country) => country.id === selectedCountry)
@@ -96,9 +178,9 @@ const RegisterForm: React.FC = () => {
                         </select>
                     </div>
                 )}
-            </div>
-            {error && <div>{error}</div>}
-            <button onClick={handleRegister}>S'inscrire</button>
+                {errors.general && <div>{errors.general}</div>}
+                <button type="submit">S'inscrire</button>
+            </form>
         </div>
     );
 }
